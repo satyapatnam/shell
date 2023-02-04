cda0cb3e9333a8f324e7160f5ffa463c76e9ac1e
satya <satyapatnam@gmail.com>
2023-02-04 22:19:46 +0000

    adding shell topics

---
+++ varsh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Variables :assign name to a set of data called variables
+# Declare a varible
+# VARNAME=DATA(NO SPACES)
+a=10
+#Access varibles -$varname-meaning $ symbol prefixing it
+echo A - $a
+# a=10 is hardcoded manually,however we need the data dynamcally
+#dynamically you can get in two ways
+#1.user input
+#2.script find out in two ways
+#command substituion
+#VARNAME=$(COMMAND)
+c=20
+#ACCESS VARIBLE FROM SHELL
+echo C - $c
+
